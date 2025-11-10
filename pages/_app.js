4994042cf16dd10b8f173221e4596deb7a280f40
rakesh-gupta29 <rakesh.rkgupta29@gmail.com>
2025-11-10 17:52:11 +0530

    added the initial boilerplate

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
